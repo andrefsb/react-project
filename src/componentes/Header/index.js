6158f8c5def30a509c518ba3a6bef289f33166bd
André Sampaio <andresampaio@id.uff.br>
2023-07-24 17:21:44 -0300

    Containers for header and app

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -1,15 +1,21 @@
 import Logo from '../logo';
 import IconesHeader from '../IconesHeader';
 import OpcoesHeader from '../OpcoesHeader';
-import './estilo.css';
+import styled from 'styled-components';
+
+const HeaderContainer = styled.header`
+        background-color: #FFF;
+        display: flex;
+        justify-content: center;
+`
 
 function Header(){
     return(
-        <header className='App-header'>
+        <HeaderContainer>
             <Logo/>
             <OpcoesHeader/>
             <IconesHeader/> 
-        </header>
+        </HeaderContainer>
     )
 };
 
